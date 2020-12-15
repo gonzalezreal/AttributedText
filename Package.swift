9fbf26ec2b173a1cbee7ef185431d5bfce4ef753
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-12-15 19:20:46 +0100

    Add Makefile

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "AttributedText",
+    platforms: [
+            .macOS(.v10_12),
+            .iOS(.v11),
+            .tvOS(.v11),
+            .watchOS(.v3),
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
