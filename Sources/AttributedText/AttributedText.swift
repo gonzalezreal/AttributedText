a104f33e2b305645393cd074b3717d307c26a4bc
Guille Gonzalez <guillermo.gonzalez@me.com>
2021-10-04 15:09:03 +0200

    Relax platform requirements and allow overriding how links are opened

--- Sources/AttributedText/AttributedText.swift
+++ Sources/AttributedText/AttributedText.swift
@@ -1,33 +1,40 @@
 #if !os(watchOS)
     import SwiftUI
 
-    @available(macOS 11.0, iOS 14.0, tvOS 14.0, *)
+    @available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
     public struct AttributedText: View {
-        @StateObject private var textViewStore = TextViewStore()
+        @StateObject private var textSizeViewModel = TextSizeViewModel()
 
         private let attributedText: NSAttributedString
+        private let openLink: ((URL) -> Void)?
 
-        public init(_ attributedText: NSAttributedString) {
+        public init(_ attributedText: NSAttributedString, openLink: ((URL) -> Void)? = nil) {
             self.attributedText = attributedText
+            self.openLink = openLink
+        }
+
+        public init(openLink: ((URL) -> Void)? = nil, attributedText: () -> NSAttributedString) {
+            self.init(attributedText(), openLink: openLink)
         }
 
         public var body: some View {
             GeometryReader { geometry in
-                TextViewWrapper(
+                AttributedTextImpl(
                     attributedText: attributedText,
                     maxLayoutWidth: geometry.maxWidth,
-                    textViewStore: textViewStore
+                    textSizeViewModel: textSizeViewModel,
+                    openLink: openLink
                 )
             }
             .frame(
-                idealWidth: textViewStore.intrinsicContentSize?.width,
-                idealHeight: textViewStore.intrinsicContentSize?.height
+                idealWidth: textSizeViewModel.textSize?.width,
+                idealHeight: textSizeViewModel.textSize?.height
             )
             .fixedSize(horizontal: false, vertical: true)
         }
     }
 
-    @available(macOS 11.0, iOS 14.0, tvOS 14.0, *)
+    @available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
     private extension GeometryProxy {
         var maxWidth: CGFloat {
             size.width - safeAreaInsets.leading - safeAreaInsets.trailing
--- Tests/AttributedTextTests/AttributedTextTests.swift
+++ Tests/AttributedTextTests/AttributedTextTests.swift
@@ -5,15 +5,28 @@
 
     import AttributedText
 
-    @available(iOS 14.0, tvOS 14.0, *)
+    @available(iOS 13.0, tvOS 13.0, *)
     final class AttributedTextTests: XCTestCase {
         struct TestView: View {
             var body: some View {
-                AttributedText(makeAttributedString())
-                    .background(Color.gray.opacity(0.5))
-                    .padding()
+                AttributedText {
+                    let result = NSMutableAttributedString(
+                        string: """
+                        The Adventures of Sherlock Holmes
+                        I had called upon my friend, Mr. Sherlock Holmes, one day in the autumn of last year and found him in deep conversation with a very stout, florid-faced, elderly gentleman with fiery red hair.
+                        """
+                    )
+
+                    result.addAttributes([.font: UIFont.preferredFont(forTextStyle: .title2)], range: NSRange(location: 0, length: 33))
+                    result.addAttributes([.font: UIFont.preferredFont(forTextStyle: .body)], range: NSRange(location: 33, length: 192))
+                    return result
+                }
+                .background(Color.gray.opacity(0.5))
+                .padding()
             }
         }
+
+        private let precision: Float = 0.99
 
         #if os(iOS)
             private let layout = SwiftUISnapshotLayout.device(config: .iPhone8)
@@ -25,33 +38,20 @@
 
         func testHeight() {
             let view = TestView()
-            assertSnapshot(matching: view, as: .image(layout: layout), named: platformName)
+            assertSnapshot(matching: view, as: .image(precision: precision, layout: layout), named: platformName)
         }
 
         func testLineLimit() {
             let view = TestView()
                 .lineLimit(2)
-            assertSnapshot(matching: view, as: .image(layout: layout), named: platformName)
+            assertSnapshot(matching: view, as: .image(precision: precision, layout: layout), named: platformName)
         }
 
         func testTruncationMode() {
             let view = TestView()
                 .lineLimit(2)
                 .truncationMode(.middle)
-            assertSnapshot(matching: view, as: .image(layout: layout), named: platformName)
+            assertSnapshot(matching: view, as: .image(precision: precision, layout: layout), named: platformName)
         }
-    }
-
-    private func makeAttributedString() -> NSAttributedString {
-        let result = NSMutableAttributedString(
-            string: """
-            The Adventures of Sherlock Holmes
-            I had called upon my friend, Mr. Sherlock Holmes, one day in the autumn of last year and found him in deep conversation with a very stout, florid-faced, elderly gentleman with fiery red hair.
-            """
-        )
-
-        result.addAttributes([.font: UIFont.preferredFont(forTextStyle: .title2)], range: NSRange(location: 0, length: 33))
-        result.addAttributes([.font: UIFont.preferredFont(forTextStyle: .body)], range: NSRange(location: 33, length: 192))
-        return result
     }
 #endif
